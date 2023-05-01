d4a3ef3d9c757d2ae8b8e4ba6641c20e671026b5
Vasilij U <vasya-1001@yandex.ru>
2023-05-01 21:49:33 +0500

    Zadacha 41 completed

--- Z41/Program.cs
+++ Z41/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// ввод чисел пользователем
+
+int input(string message)
+{
+    System.Console.Write(message);
+    string line = System.Console.ReadLine();
+    int result = Convert.ToInt32(line);
+    return result;
+}
+
+// подсчет положительных чисел
+
+int count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+// Введение чисел в массив
+
+int [] InputArray(int length)
+{
+    int [] array = new int [length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = input($"Введите {i+1}-й этемент: ");
+    }
+    return array;
+}
+
+// печать массива
+
+void PrintArray (int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int length = input("Введите количество элементов: ");
+int [] array;
+array = InputArray(length);
+PrintArray(array);
+
+System.Console.WriteLine($"Количество чисел больше нуля - {count(array)}");
